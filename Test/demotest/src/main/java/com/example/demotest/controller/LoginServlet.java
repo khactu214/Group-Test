f930d85d8cb3c3d74622aa35eaac484549420ade
phamvietkhanh9 <61862710+phamvietkhanh9@users.noreply.github.com>
2020-07-24 18:49:10 +0700

    Update LoginServlet.java

--- Test/demotest/src/main/java/com/example/demotest/controller/LoginServlet.java
+++ Test/demotest/src/main/java/com/example/demotest/controller/LoginServlet.java
@@ -11,7 +11,8 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-@WebServlet(name = "LoginServlet")
+@Controller
+@WebServlet(name = "/login")
 public class LoginServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
